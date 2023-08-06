75b97aa5d9638cfbc4696fcbe476726074d11cd3
phamdt203 <phamdt203@gmail.com>
2023-08-06 09:34:33 +0700

    update

--- predict.py
+++ predict.py
@@ -1,35 +1,41 @@
 import cv2
 import torch
 import pickle
+import numpy as np
 from torchvision import transforms
 from PIL import Image
 from parameters import *
 from faceDetector import *
 import torch.nn.functional as F
 from model import mobilenet_v2
+from matplotlib import cm
 
 def img_to_encoding(image_tensor, model, device, preprocess):
+    image_tensor = Image.fromarray(image_tensor).convert("RGB")
     image_tensor = preprocess(image_tensor).unsqueeze(0).to(device)
     with torch.no_grad():
         return model(image_tensor)
 
 def similarity_score(firstEmbedding, secondEmbedding):
-    return F.cosine_similarity(firstEmbedding, secondEmbedding)
+    return F.pairwise_distance(firstEmbedding, secondEmbedding)
 
 def faceDetector(faceDetector, image_path):    
     image = cv2.imread(image_path)
     gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    faceRects = faceDetector.detect(gray_img)
+    faceRects = faceDetector.detectMultiScale(gray_img, 1.1, 9)
+    face = np.zeros((image.shape[0], image.shape[1]))
     for (x, y, w, h) in faceRects:
         left, top, right, bottom = x, y, x + w, y + h
         face = image[top : bottom, left : right]
-    return torch.from_numpy(face), faceRects
+    return face, faceRects
 
 def Recognized(testEmbedding, database):
     min_dist = 1000
     face_name = ""
     for face in database.keys():
-        similarityScore = similarity_score(database[face], testEmbedding)
+        if len(database[face]) < 1:
+            continue
+        similarityScore = similarity_score(database[face][0], testEmbedding)
         if similarityScore < min_dist:
             min_dist = similarityScore
             face_name = face
@@ -37,23 +43,28 @@ def Recognized(testEmbedding, database):
 
 def faceRecognition(database, paths, device, model, preprocess):
     for face in list(database)[:5]:
+        paths[face] = paths[face].replace('\\', '/')
         if os.path.exists(paths[face]):
-            image = Image.open(os.listdir(paths[face].replace('\\', '/'))[0]).convert("RGB")
-            fd = faceDetector('haarcascade_frontalface_default.xml')
-            image_tensor, faceRects = faceDetector(fd, paths[face])
+            image_path = os.path.join(paths[face], os.listdir(paths[face])[0])
+            image = Image.open(image_path).convert("RGB")
+            fd = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+            image_tensor, faceRects = faceDetector(fd, image_path)
             testEmbedding = img_to_encoding(image_tensor, model, device, preprocess)
             face_name = Recognized(testEmbedding, database)
+            image = np.array(image)
             for (x, y, w, h) in faceRects:
                 cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
                 font_scale = 0.75
                 font_thickness = 2
                 text_size = cv2.getTextSize(face_name, cv2.FONT_HERSHEY_SIMPLEX, font_scale, font_thickness)
-                text_x = x + (w - text_size[0]) // 2
-                text_y = y - 10
+                text_x = int(x + (w - text_size[0][0]) // 2)
+                text_y = int(y - 10)
                 cv2.putText(image, face_name, (text_x, text_y), cv2.FONT_HERSHEY_SIMPLEX, font_scale, (0, 255, 0), font_thickness)
             cv2.imwrite(f"output/{face_name}.jpg", image)
 
+
 model = mobilenet_v2()
+model.load_state_dict(torch.load("facenet_model.pth"))
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = model.to(device)
 preprocess = transforms.Compose([
